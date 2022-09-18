8e5b5af64997e0dda59e0158718d32dd3cf7b790
reach2yuga1 <100332937+reach2yuga1@users.noreply.github.com>
2022-09-18 13:39:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 
 streamlit.title("my parents healthy dinner")
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
